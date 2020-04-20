7ca6442a1446cdb8aa17db75ba81ecc27b6cb657
ievgen.zubkov <ievgen.zubkov@ihsmarkit.com>
2020-04-20 15:42:16 +0300

    Add lobby component

--- src/Neon/ClientApp/src/app/home/home.component.ts
+++ src/Neon/ClientApp/src/app/home/home.component.ts
@@ -1,63 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import * as signalR from "@microsoft/signalr";
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent implements OnInit  {
-  divMessages: HTMLDivElement;
-  tbMessage: HTMLInputElement;
-  btnSend: HTMLButtonElement;
-  connection: signalR.HubConnection;
+export class HomeComponent {
 
-  constructor(private httpClient: HttpClient) {
-    
-  }
-
-  ngOnInit(): void {
-    this.divMessages = document.querySelector("#divMessages");
-    this.tbMessage = document.querySelector("#tbMessage");
-    this.btnSend = document.querySelector("#btnSend");
-    this.connection = new signalR.HubConnectionBuilder()
-      .withUrl("/homeHub")
-      .build();
-
-    this.connection.on("messageReceived", (username: string, message: string) => {
-      console.log(username, message);
-    });
-
-    this.connection.start().catch(err => document.write(err));
-  }
-
-  send() {
-    window['FB'].getLoginStatus(response => {
-      if (response.status === 'connected') {
-        // The user is logged in and has authenticated your
-        // app, and response.authResponse supplies
-        // the user's ID, a valid access token, a signed
-        // request, and the time the access token 
-        this.sendWithFbUser();
-      } else {
-        window['FB'].login((response) => {
-          console.log('login response', response);
-          if (response.authResponse) {
-            this.sendWithFbUser();
-          } else {
-            console.log('User login failed');
-          }
-        }, { scope: 'email' });
-      }
-    });
-  }
-
-  sendWithFbUser() {
-    window['FB'].api('/me', {
-      fields: 'last_name, first_name, email'
-    }, (userInfo) => {
-      this.connection.send("newMessage", userInfo.email, this.tbMessage.value)
-        .then(() => this.tbMessage.value = "");
-    });
-  }
 }
---
+++ src/Neon/ClientApp/src/app/lobby/lobby.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import * as signalR from "@microsoft/signalr";
+
+@Component({
+    selector: 'app-lobby',
+    templateUrl: './lobby.component.html'
+})
+export class LobbyComponent implements OnInit {
+  divMessages: HTMLDivElement;
+  tbMessage: HTMLInputElement;
+  btnSend: HTMLButtonElement;
+  connection: signalR.HubConnection;
+
+  ngOnInit(): void {
+    this.divMessages = document.querySelector("#divMessages");
+    this.tbMessage = document.querySelector("#tbMessage");
+    this.btnSend = document.querySelector("#btnSend");
+    this.connection = new signalR.HubConnectionBuilder()
+      .withUrl("/homeHub")
+      .build();
+
+    this.connection.on("messageReceived", (username: string, message: string) => {
+      console.log(username, message);
+    });
+
+    this.connection.start().catch(err => document.write(err));
+  }
+
+  send() {
+    window['FB'].getLoginStatus(response => {
+      if (response.status === 'connected') {
+        // The user is logged in and has authenticated your
+        // app, and response.authResponse supplies
+        // the user's ID, a valid access token, a signed
+        // request, and the time the access token 
+        this.sendWithFbUser();
+      } else {
+        window['FB'].login((response) => {
+          console.log('login response', response);
+          if (response.authResponse) {
+            this.sendWithFbUser();
+          } else {
+            console.log('User login failed');
+          }
+        }, { scope: 'email' });
+      }
+    });
+  }
+
+  sendWithFbUser() {
+    window['FB'].api('/me', {
+      fields: 'last_name, first_name, email'
+    }, (userInfo) => {
+      this.connection.send("newMessage", userInfo.email, this.tbMessage.value)
+        .then(() => this.tbMessage.value = "");
+    });
+  }
+}
